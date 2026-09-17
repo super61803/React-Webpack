568eb4b69018adf5aea7b3da7e7659711e4ab58a
super61803 <newest.star1990@gmail.com>
2026-09-17 09:17:42 -0500

    feat: copy public static assets into Webpack build output
    Add copy-webpack-plugin so files in public/ ship with production builds the way CRA did.

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
 
 module.exports = (env, argv) => {
   const isProduction = argv.mode === "production";
@@ -57,6 +58,18 @@ module.exports = (env, argv) => {
     plugins: [
       new HtmlWebpackPlugin({
         template: path.resolve(__dirname, "public/index.html"),
+      }),
+      new CopyWebpackPlugin({
+        patterns: [
+          {
+            from: path.resolve(__dirname, "public"),
+            to: path.resolve(__dirname, "build"),
+            globOptions: {
+              ignore: ["**/index.html"],
+            },
+            noErrorOnMissing: true,
+          },
+        ],
       }),
     ],
     devServer: {
