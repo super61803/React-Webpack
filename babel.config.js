636b26bb177baa1cbbbb12b27f1768b8e103a255
super61803 <newest.star1990@gmail.com>
2026-09-21 06:23:15 -0500

    chore: harden Babel config for Webpack tree-shaking
    Enable api.cache, preset-env bugfixes, and modules:false so Webpack owns ES module bundling.

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,23 @@
-module.exports = {
-  presets: [
-    "@babel/preset-env",
-    [
-      "@babel/preset-react",
-      {
-        runtime: "automatic",
-      },
+module.exports = (api) => {
+  api.cache(true);
+
+  return {
+    presets: [
+      [
+        "@babel/preset-env",
+        {
+          bugfixes: true,
+          // Let Webpack handle ES modules for better tree-shaking.
+          modules: false,
+        },
+      ],
+      [
+        "@babel/preset-react",
+        {
+          runtime: "automatic",
+        },
+      ],
+      "@babel/preset-typescript",
     ],
-    "@babel/preset-typescript",
-  ],
+  };
 };
